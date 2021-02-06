808eb69444aec1a1691cd27f5877bc49d09b9dbc
Omar Branez <omar.branez@gmail.com>
2021-02-06 16:39:23 -0500

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,17 @@
+# def reverse_each_word(sentence)
+#     backwards = sentence.split(" ")
+#     backwards.each do |words| 
+#         words.reverse!
+#     end
+#     return backwards.join(" ")
+# end
+
+# def reverse_each_word(sentence)
+#     backwards = sentence.split(" ")
+#     backwards.collect {|words| words.reverse!}
+#     return backwards.join(" ")
+# end
+
+def reverse_each_word(sentence)
+    sentence.split.collect {|words| words.reverse!}.join(" ")
+end
